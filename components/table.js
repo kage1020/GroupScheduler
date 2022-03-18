c08c8d825c86462249d25028df2dc9e9ed51970c
kage1020 <4620087@ed.tus.ac.jp>
2022-03-18 21:03:52 +0900

    add status table

---
+++ components/table.js
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159),
+  createData('Ice cream sandwich', 237),
+  createData('Eclair', 262, 16.0),
+  createData('Cupcake', 305),
+  createData('Gingerbread'),
+];
+
+export default function BasicTable({ liff }) {
+  const [results, setResult] = useState({
+    os: ''
+  });
+
+  useEffect(() => {
+    if (!liff) return;
+    if (!liff.isLoggedIn()) {
+      liff.login();
+    }
+    setResult({
+      ...results,
+      ...{
+        os: liff.getOS(),
+        lang: liff.getLanguage(),
+        version: liff.getVersion(),
+        lineVersion: `${liff.getLineVersion()}`,
+        inClient: `${liff.isInClient()}`,
+        loggedIn: `${liff.isLoggedIn()}`
+      }
+    })
+  }, [liff, results])
+
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label='simple table'>
+        <TableHead>
+          <TableRow>
+            <TableCell>item</TableCell>
+            <TableCell>result</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {Object.keys(results).map((item, index) => (
+            <TableRow key={index}>
+              <TableCell>{item}</TableCell>
+              <TableCell>{results[item]}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,32 @@
+import { useState, useEffect } from 'react';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  const [liffObject, setObject] = useState(null);
+  const [liffError, setError] = useState(null);
+
+  useEffect(() => {
+    import('@line/liff').then((liff) => {
+      liff
+        .init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID })
+        .then(() => {
+          console.log('liff.init() done');
+          setObject(liff);
+        })
+        .catch((error) => {
+          console.log(`liff.init() failed: ${error}`);
+          if (!process.env.NEXT_PUBLIC_LIFF_ID) {
+            console.info(
+              'LIFF Starter: Please make sure that you provided `LIFF_ID` as an environmental variable.'
+            );
+          }
+          setError(error.toString());
+        });
+    });
+  }, []);
+
+  pageProps.liff = liffObject;
+  pageProps.liffError = liffError;
   return <Component {...pageProps} />
 }
 
